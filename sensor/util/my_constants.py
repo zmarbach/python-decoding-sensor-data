1d00be4e42c02e27c6bc4fb3d5a20f0a478562e4
Marbach <ZJM0411@daugherty.com>
2021-03-03 15:39:20 -0600

    refactoring

---
+++ sensor/util/my_constants.py
@@ -0,0 +1,13 @@
+# Common field names
+ID = 'id'
+DATE = 'date'
+TEMPERATURE = 'temperature'
+HUMIDITY = 'humidity'
+ENERGY_USAGE = 'energy_usage'
+PARTICULATE = 'particulate'
+AREA = "area"
+
+# Common date formats
+MDY_FORMAT = "%m/%d/%y"
+
+
