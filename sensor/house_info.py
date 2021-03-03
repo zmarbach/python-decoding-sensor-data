d8d775ae8927a7c462ce0cb96cfbc79433eda399
Marbach <ZJM0411@daugherty.com>
2021-03-03 12:40:52 -0600

    update data variable name

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,14 +1,15 @@
 from datetime import date, datetime
 
+
 class HouseInfo:
     def __init__(self, data):
         # data is a list of dicts
-        self._data = data
+        self.data = data
 
     def get_data_by_area(self, field, rec_area=0):
         field_data = []
         # Add record to field_data if the value of rec_area is either 0 or equal to the record's area
-        for record in self._data:
+        for record in self.data:
             if rec_area == 0:
                 field_data.append(record[field])
             elif rec_area == int(record['area']):
@@ -17,7 +18,7 @@ class HouseInfo:
 
     def get_data_by_date(self, field, rec_date=date.today()):
         field_data = []
-        for record in self._data:
+        for record in self.data:
             if date.strftime(rec_date, "%m/%d/%y") == record['date']:
                 field_data.append(record[field])
         return field_data
